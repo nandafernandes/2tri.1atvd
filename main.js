62beb759fffaf3fe1044d88a80670e411b1d7218
Nanda <102810392+nandafernandes@users.noreply.github.com>
2024-06-05 09:13:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var caixaPrincipal = document.querySelector(".caixa-principal");
+var caixaPerguntas = document.querySelector(".caixa-perguntas");
+var caixaAlternativas = document.querySelector(".caixa-alternativas");
+var caixaResultado = document.querySelector(".caixa-resultado");
+var textoResultado = document.querySelector(".texto-resultado");
+var perguntas = [
+    {
+        enunciado: "A IA pode automatizar tarefas repetitivas.",
+        alternativas: [
+            "Vai eliminar tarefas monotonas",
+            "Vai tirar muitos empregos"
+        ]
+    },
+    {
+        enunciado: "A IA pode tomar decisões baseadas em dados.",
+        alternativas: [
+            "Maravilhoso",
+            "Assustador"
+        ]
+    },
+    {
+        enunciado: "A IA pode substituir certos empregos.",
+        alternativas: [
+            "Não vejo problemas",
+            "É o fim dos empregos como conhecemos hoje"
+        ]
+    },
+    {
+        enunciado: "A IA pode ajudar a prever tendências de mercado.",
+        alternativas: [
+            "Incrível",
+            "Sinistro"
+        ]
+    },
+    {
+        enunciado: "A IA pode trabalhar 24/7 sem pausas.",
+        alternativas: [
+            "Surpreendente",
+            "Mão de obra barata"
+        ]
+    }
+]
+
+
